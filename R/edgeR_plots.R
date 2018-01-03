93320fd9edc90f2ccade727aea66de5dcdceabc6
Audrey Renson <audrey.o.renson@gmail.com>
2018-01-03 16:43:55 -0500

    correcting usage of outside packages

--- R/edgeR_plots.R
+++ R/edgeR_plots.R
@@ -8,7 +8,7 @@
 #' @param pseq object of class \code{phyloseq}
 #' @param coef integer. Specifies which linear model coefficient to test (default 2).
 #' @param printIfEmpty logical. If no results pass alpha, whether or not to plot an empty plot.
-#' @param invisbl logical. Whether or not to invisibly return the \code{topTags} object returned by \code{get_edgeR_results}.
+#' @param invisbl logical. Whether or not to forgoe plotting. Either way, invisibly returns the \code{topTags} object returned by \code{get_edgeR_results}.
 #' @param color character. Variable  to color by, from either the \code{tax_table} or the \code{topTags$table}.
 #' @param sortby character. Variable to sort the plot by, from either the \code{tax_table} or the \code{topTags$table}.
 #' @param ... further arguments passed to \code{get_edgeR_results}.
@@ -43,13 +43,14 @@ plot_edgeR <- function(formla, ttle=NULL, varname=NULL,
 
   #drop NA
 
-  #sort genus factor levels by log fold change
+  #sort genus factor levels by log fold change if sortby is missing
   if(missing(sortby)) {
-    dat$Genus %<>% as.character %>% factor(levels = unique(dat$Genus[order(dat$logFC, decreasing = TRUE)]))
+    ord <- order(dat$logFC, decreasing = TRUE)
   } else {
-    dat$Genus %<>% as.character %>% factor(levels = unique(dat$Genus[order(dat[[sortby]], dat$logFC, decreasing=TRUE)]))
+    ord <- order(dat[[sortby]], dat$logFC, decreasing=TRUE)
   }
-
+  dat$Genus <- as.character(dat$Genus)
+  dat$Genus <- factor(dat$Genus, levels = unique(dat$Genus[ord]))
 
 
   #create title
@@ -71,23 +72,23 @@ plot_edgeR <- function(formla, ttle=NULL, varname=NULL,
   phyla_colors <- c('#00B2FFFF','#FF9900FF','#FF0000FF','#0DFF00FF','#00FF9FFF','#A600FFFF','#B9FF00FF','#FF00ACFF','#0006FFFF')
 
   #plot
-  p <- dat %>%
-    ggplot(aes(x=logFC, y=Genus, color=eval(as.name(color)))) +
-    geom_vline(xintercept = 0.0, color = "gray", size = 0.5) +
-    geom_point(size=3) +
-    guides(color=guide_legend(title=color)) +
-    theme_light() +
-    theme(axis.text.x = element_text(angle = -90, hjust = 0, vjust=0.5),
-          axis.text.y = element_text(face = "italic"),
-          axis.text=element_text(size=12),
-          legend.text = element_text(size=12),
-          legend.title = element_text(size=14),
-          title = element_text(size=15)) +
-    scale_y_discrete(position="right") +
-    labs(x="Log10-fold change") +
-    ggtitle(ttle)
+  p <-
+    ggplot2::ggplot(dat, ggplot2::aes(x=logFC, y=Genus, color=eval(as.name(color)))) +
+    ggplot2::geom_vline(xintercept = 0.0, color = "gray", size = 0.5) +
+    ggplot2::geom_point(size=3) +
+    ggplot2::guides(color=ggplot2::guide_legend(title=color)) +
+    ggplot2::theme_light() +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = -90, hjust = 0, vjust=0.5),
+          axis.text.y = ggplot2::element_text(face = "italic"),
+          axis.text = ggplot2::element_text(size=12),
+          legend.text = ggplot2::element_text(size=12),
+          legend.title = ggplot2::element_text(size=14),
+          title = ggplot2::element_text(size=15)) +
+    ggplot2::scale_y_discrete(position="right") +
+    ggplot2::labs(x="Log10-fold change") +
+    ggplot2::ggtitle(ttle)
   #add the colors set permanently to phyla (for multiple plots)
-  if(color=="Phylum") p <- p + scale_color_manual(values=phyla_colors)
+  if(color=="Phylum") p <- p + ggplot2::scale_color_manual(values=phyla_colors)
   if(!invisbl) print(p)
   invisible(dat)
 }
@@ -98,7 +99,8 @@ edger_summary_plot <- function(list_models, ttle="Significant genera by sociodem
 
   require(magrittr)
   require(dplyr)
-  list_models %<>% nullify_nondataframes %>% drop_nulls_from_list
+  list_models <- nullify_nondataframes(list_models)
+  list_models <- drop_nulls_from_list(list_models)
 
   #create a data frame with a column of all genera appearing in any of the models
   all_sig_OTUs <- data.frame(genus =
@@ -161,23 +163,21 @@ edger_summary_plot <- function(list_models, ttle="Significant genera by sociodem
     rownames(matrix_OTU_appears_in_model) <- all_sig_OTUs$genus
 
     #bind it to the data frame
-    all_sig_OTUs %<>%
-      cbind(as.data.frame(matrix_OTU_appears_in_model)) %>%
-      arrange(desc(count)) %>% # sort by count of appearances
-      select(-count) # remove the count variable, not necessary
-
+    all_sig_OTUs <- cbind(all_sig_OTUs, as.data.frame(matrix_OTU_appears_in_model))
+    all_sig_OTUs <- dplyr::arrange(all_sig_OTUs, desc(count)) # sort by count of appearances
+    all_sig_OTUs <- dplyr::select(all_sig_OTUs, -count) # remove the count variable, not necessary
   }
 
   melted_appearances <- reshape2::melt(matrix_OTU_appears_in_model)
   melted_appearances$X1 <- factor(melted_appearances$X1,
                                   levels = all_sig_OTUs$genus)
-  melted_appearances %>%
-    ggplot(aes(x=X1, y=X2, fill=value)) +
-    geom_tile() +
-    scale_fill_gradient2(name="Appeared") +
-    theme(axis.text.x = element_text(angle = 45, vjust=1, hjust = 1),
+
+  ggplot2::ggplot(melted_appearances, ggplot2::aes(x=X1, y=X2, fill=value)) +
+    ggplot2::geom_tile() +
+    ggplot2::scale_fill_gradient2(name="Appeared") +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, vjust=1, hjust = 1),
           legend.position = "none") +
-    labs(y="Sociodemographic Variable", x="Genus", title=ttle)
+    ggplot2::labs(y="Sociodemographic Variable", x="Genus", title=ttle)
 
 }
 
@@ -200,13 +200,12 @@ edger_logfc_tileplot <- function(list_list_models) {
   dfs <- lapply(seq_along(list_list_models), function(i) edger_logfc_tileplot_df(list_list_models[[i]], oligo_name = names(list_list_models)[i]))
   df_tile <- do.call(rbind, dfs)
 
-
-  df_tile %>%
-    ggplot(aes(x=OTU, y=coef, fill=logFC)) +
-    geom_tile() + theme_minimal() +
-    theme(axis.text.x = element_text(angle = 45, vjust=1, hjust = 1)) +
-    scale_fill_gradient2(limits=c(-3,3.5),low = "seagreen4", mid = "white", high = "purple3") +
-    facet_wrap(~oligo_name,ncol=1, dir="h", scales="free")
+  ggplot2::ggplot(df_tile, ggplot2::aes(x=OTU, y=coef, fill=logFC)) +
+    ggplot2::geom_tile() +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, vjust=1, hjust = 1)) +
+    ggplot2::scale_fill_gradient2(limits=c(-3,3.5),low = "seagreen4", mid = "white", high = "purple3") +
+    ggplot2::facet_wrap(~oligo_name,ncol=1, dir="h", scales="free")
 }
 
 edger_logfc_tileplot_df <- function(list_models, oligo_name) {
@@ -232,7 +231,6 @@ edger_logfc_tileplot_df <- function(list_models, oligo_name) {
   df_tile <- do.call(rbind, df_tile)
   df_tile$OTU <- factor(df_tile$OTU, labels = paste0("O", 1:nlevels(df_tile$OTU)))
   df_tile$oligo_name <- oligo_name
-
 
   df_tile
 }
